2826d0e91b9289b5df6ab750cb4ce87ec2432ffb
bumbu <bmbalex@gmail.com>
2013-05-18 18:41:29 +0300

    add testing facility

---
+++ app/js/application.js
@@ -0,0 +1,11 @@
+$(function () {
+  var $textarea = $('#timeline-textarea')
+    , $redraw = $('#timeline-redraw')
+    , $timeline = $('#timeline-body')
+
+  $redraw.on('click', function (ev) {
+    ev.preventDefault()
+
+    $timeline.timeline('redraw', JSON.parse($textarea.val()));
+  })
+})
